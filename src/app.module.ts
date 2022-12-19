b5c603ea5b75be70692a43d875409f210990f696
Jobin T v <jobintv@focaloid.com>
2022-12-19 13:08:52 +0530

    feat: Caching with redis-store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
-import { CacheInterceptor, CacheModule } from '@nestjs/common/cache';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import * as redisStrore from 'cache-manager-redis-store';
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    CacheModule.register({
+      store: redisStrore,
+      socket: { host: 'localhost', port: 6379 },
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
